4e32442da1281d0956022322f5f8ec339a6b700a
Krystan HuffMenne <kmenne+github@gmail.com>
2025-09-04 14:25:41 -0700

    Clean up GET builders

--- packages/shared-data/src/builders/todo.ts
+++ packages/shared-data/src/builders/todo.ts
@@ -1,65 +1,58 @@
-import { mergeOptions } from '@workspace/shared-utils';
-
+import type { ReactiveDataDocument } from '@warp-drive/core/reactive';
 import { withReactiveResponse } from '@warp-drive/core/request';
-import type { QueryRequestOptions } from '@warp-drive/core/types/request';
-import type { CollectionResourceDataDocument } from '@warp-drive/core/types/spec/document';
-import { buildBaseURL } from '@warp-drive/utilities';
-import {
-  deleteRecord,
-  findRecord,
-  query,
-  updateRecord,
-} from '@warp-drive/utilities/json-api';
+import type { RequestInfo } from '@warp-drive/core/types/request';
+import { buildBaseURL, buildQueryParams } from '@warp-drive/utilities';
+import { deleteRecord, updateRecord } from '@warp-drive/utilities/json-api';
 
 import type { SavedTodo, TodoAttributes } from '../types/index.ts';
 
-// GET
-export type GetTodosResult = CollectionResourceDataDocument<SavedTodo>;
+// GET /todo
+export type GetTodosResult = ReactiveDataDocument<SavedTodo[]>;
 
-export function getAllTodos(): QueryRequestOptions<GetTodosResult> {
-  const requestInfo = query<SavedTodo>('todo', {}, { resourcePath: 'todo' });
-  requestInfo.cacheOptions = mergeOptions(requestInfo.cacheOptions, {
-    types: ['todo'],
+export function getAllTodos(): RequestInfo<GetTodosResult> {
+  return withReactiveResponse<SavedTodo[]>({
+    method: 'GET',
+    url: buildBaseURL({ resourcePath: 'todo' }),
+
+    // Query ops are invalidated by createRecord, updateRecord, deleteRecord ops
+    op: 'query',
+    cacheOptions: { types: ['todo'] },
   });
-  return requestInfo;
 }
 
-export function getCompletedTodos(): QueryRequestOptions<GetTodosResult> {
-  const requestInfo = query<SavedTodo>(
-    'todo',
-    { completed: true },
-    { resourcePath: 'todo' },
-  );
-  requestInfo.cacheOptions = mergeOptions(requestInfo.cacheOptions, {
-    types: ['todo'],
-  });
-  return requestInfo;
-}
-
-export function getActiveTodos(): QueryRequestOptions<GetTodosResult> {
-  const requestInfo = query<SavedTodo>(
-    'todo',
-    { completed: false },
-    { resourcePath: 'todo' },
-  );
-  requestInfo.cacheOptions = mergeOptions(requestInfo.cacheOptions, {
-    types: ['todo'],
-  });
-  return requestInfo;
-}
-
-// FIXME: Unused
-export function getTodoById(id: string) {
-  return findRecord<SavedTodo>('todo', id, { resourcePath: 'todo' });
-}
-
-// POST
-export function createTodo(attributes: TodoAttributes) {
+// GET /todo?completed=true
+export function getCompletedTodos(): RequestInfo<GetTodosResult> {
   const url = buildBaseURL({ resourcePath: 'todo' });
+  const queryString = buildQueryParams({ completed: true });
 
+  return withReactiveResponse<SavedTodo[]>({
+    method: 'GET',
+    url: `${url}?${queryString}`,
+
+    op: 'query',
+    cacheOptions: { types: ['todo'] },
+  });
+}
+
+// GET /todo?completed=false
+export function getActiveTodos(): RequestInfo<GetTodosResult> {
+  const url = buildBaseURL({ resourcePath: 'todo' });
+  const queryString = buildQueryParams({ completed: false });
+
+  return withReactiveResponse<SavedTodo[]>({
+    method: 'GET',
+    url: `${url}?${queryString}`,
+
+    op: 'query',
+    cacheOptions: { types: ['todo'] },
+  });
+}
+
+// POST /todo
+export function createTodo(attributes: TodoAttributes) {
   return withReactiveResponse<SavedTodo>({
-    url,
     method: 'POST',
+    url: buildBaseURL({ resourcePath: 'todo' }),
 
     // This op plus cacheOptions -> will invalidate any op: 'query' requests for 'todo'
     op: 'createRecord',
