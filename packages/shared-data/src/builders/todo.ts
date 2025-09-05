4eac55b577e91d6e1def870070ea7e6ec2522041
Krystan HuffMenne <kmenne+github@gmail.com>
2025-09-04 20:48:03 -0700

    Add some internal documentation re: caching

--- packages/shared-data/src/builders/todo.ts
+++ packages/shared-data/src/builders/todo.ts
@@ -17,19 +17,26 @@ import type { SavedTodo, TodoAttributes } from '../types/index.ts';
 export type ReactiveSavedTodoDocument = ReactiveDataDocument<SavedTodo>;
 export type ReactiveSavedTodosDocument = ReactiveDataDocument<SavedTodo[]>;
 
-// GET /todo
+/**
+ * GET /todo
+ */
 export function getAllTodos(): RequestInfo<ReactiveSavedTodosDocument> {
   return withReactiveResponse<SavedTodo[]>({
     method: 'GET',
     url: buildBaseURL({ resourcePath: 'todo' }),
 
-    // Query ops are invalidated by createRecord, updateRecord, deleteRecord ops
+    // Adding the 'query' OpCode and specifying the 'todo' type in
+    // `cacheOptions` tells the `DefaultCachePolicy` in our store to
+    // automatically invalidate this request when any request with the
+    // 'createRecord' OpCode + 'todo' in `cacheOptions.type` succeeds.
     op: 'query',
     cacheOptions: { types: ['todo'] },
   });
 }
 
-// GET /todo?completed=true
+/**
+ * GET /todo?completed=true
+ */
 export function getCompletedTodos(): RequestInfo<ReactiveSavedTodosDocument> {
   const url = buildBaseURL({ resourcePath: 'todo' });
   const queryString = buildQueryParams({ completed: true });
@@ -42,8 +49,9 @@ export function getCompletedTodos(): RequestInfo<ReactiveSavedTodosDocument> {
     cacheOptions: { types: ['todo'] },
   });
 }
-
-// GET /todo?completed=false
+/**
+ * GET /todo?completed=false
+ */
 export function getActiveTodos(): RequestInfo<ReactiveSavedTodosDocument> {
   const url = buildBaseURL({ resourcePath: 'todo' });
   const queryString = buildQueryParams({ completed: false });
@@ -57,32 +65,36 @@ export function getActiveTodos(): RequestInfo<ReactiveSavedTodosDocument> {
   });
 }
 
-// POST /todo
+/**
+ * POST /todo
+ */
 export function createTodo(
   attributes: TodoAttributes,
 ): RequestInfo<ReactiveSavedTodoDocument> {
   return withReactiveResponse<SavedTodo>({
     method: 'POST',
     url: buildBaseURL({ resourcePath: 'todo' }),
-
-    // This op plus cacheOptions -> will invalidate any op: 'query' requests for 'todo'
-    op: 'createRecord',
-    cacheOptions: {
-      types: ['todo'],
-    },
-
     body: JSON.stringify({
       data: {
         type: 'todo',
         attributes,
       },
     }),
+
+    // Adding the 'createRecord' OpCode and specifying the 'todo' type in
+    // `cacheOptions` tells the `DefaultCachePolicy` in our store to
+    // automatically invalidate any requests with the 'query' OpCode + 'todo'
+    // in their `cacheOptions.type` when this request succeeds.
+    op: 'createRecord',
+    cacheOptions: { types: ['todo'] },
   });
 }
 
 // FIXME: Implement bulk delete
 
-// PATCH /todo/:id
+/**
+ * PATCH /todo/:id
+ */
 export function patchTodo(
   todo: SavedTodo,
   attributes: Partial<TodoAttributes>,
@@ -102,13 +114,6 @@ export function patchTodo(
   return withReactiveResponse<SavedTodo>({
     url,
     method: 'PATCH',
-
-    op: 'updateRecord',
-    data: {
-      record: key,
-    },
-    records: [key],
-
     body: JSON.stringify({
       data: {
         type: 'todo',
@@ -116,19 +121,43 @@ export function patchTodo(
         attributes,
       },
     }),
+
+    // Adding the 'updateRecord' OpCode and specifying the `ResourceKey` for
+    // this todo in the `records` array tells the `DefaultCachePolicy` in our
+    // store that when this request succeeds it should automatically patch
+    // the returned attributes into any matching resources in any cached
+    // documents for requests with the 'query' OpCode that include this
+    // record in their results when this request succeeds.
+    op: 'updateRecord',
+    records: [key],
   });
 }
 
+/**
+ * Moves a completed todo to the "completed" list and removes it from the
+ * "active" list.
+ *
+ * While the `DefaultCachePolicy` in our store will automatically patch
+ * the updated _attributes_ for any requests with the 'query' OpCode that
+ * include this record in their results, it cannot automatically move the
+ * record between different queries (e.g. active vs completed todos).
+ *
+ * This is because while these queries are subscribed to notifications for
+ * the records they return, they explicitly do not subscribe to changes in
+ * the records they don't return.
+ *
+ * Thus, we need to manually patch the request documents stored in the cache
+ * to ensure they update.
+ *
+ * @param store - The store instance
+ * @param todo - The todo that was marked completed
+ */
 export function patchCacheTodoCompleted(store: Store, todo: SavedTodo) {
   const resourceKey = keyForResource(todo);
   const completedRequestKey = keyForRequest(store, getCompletedTodos());
   const activeRequestKey = keyForRequest(store, getActiveTodos());
 
-  // We need this because while these queries are subscribed to notifications
-  // for the records they return, they explicitly do not subscribe to changes
-  // in the records they don't return. Thus, we need to manually patch the
-  // request documents stored in the cache to ensure they update.
-  // Add to completed, remove from active
+  // Add to "completed"
   store.cache.patch({
     record: completedRequestKey,
     op: 'add',
@@ -136,6 +165,7 @@ export function patchCacheTodoCompleted(store: Store, todo: SavedTodo) {
     field: 'data',
     index: 0,
   });
+  // Remove from "active"
   store.cache.patch({
     record: activeRequestKey,
     op: 'remove',
@@ -144,17 +174,31 @@ export function patchCacheTodoCompleted(store: Store, todo: SavedTodo) {
   });
 }
 
+/**
+ * Moves an active todo to the "active" list and removes it from the
+ * "completed" list.
+ *
+ * While the `DefaultCachePolicy` in our store will automatically patch
+ * the updated _attributes_ for any requests with the 'query' OpCode that
+ * include this record in their results, it cannot automatically move the
+ * record between different queries (e.g. active vs completed todos).
+ *
+ * This is because while these queries are subscribed to notifications for
+ * the records they return, they explicitly do not subscribe to changes in
+ * the records they don't return.
+ *
+ * Thus, we need to manually patch the request documents stored in the cache
+ * to ensure they update.
+ *
+ * @param store - The store instance
+ * @param todo - The todo that was marked completed
+ */
 export function patchCacheTodoActivated(store: Store, todo: SavedTodo) {
   const resourceKey = keyForResource(todo);
   const completedRequestKey = keyForRequest(store, getCompletedTodos());
   const activeRequestKey = keyForRequest(store, getActiveTodos());
 
-  // We need this because while these queries are subscribed to notifications
-  // for the records they return, they explicitly do not subscribe to changes
-  // in the records they don't return. Thus, we need to manually patch the
-  // request documents stored in the cache to ensure they update.
-
-  // Add to active, remove from completed
+  // Add to "active"
   store.cache.patch({
     record: activeRequestKey,
     op: 'add',
@@ -162,6 +206,7 @@ export function patchCacheTodoActivated(store: Store, todo: SavedTodo) {
     field: 'data',
     index: 0,
   });
+  // Remove from "completed"
   store.cache.patch({
     record: completedRequestKey,
     op: 'remove',
@@ -175,18 +220,46 @@ export function deleteTodo(todo: SavedTodo) {
   return deleteRecord(todo, { resourcePath: 'todo' });
 }
 
+// FIXME: Is this actually necessary?
+/**
+ * Removes a deleted todo from both the "active" list and "completed" lists.
+ *
+ * While the `DefaultCachePolicy` in our store will automatically patch
+ * the updated _attributes_ for any requests with the 'query' OpCode that
+ * include this record in their results, it cannot automatically move the
+ * record between different queries (e.g. active vs completed todos).
+ *
+ * This is because while these queries are subscribed to notifications for
+ * the records they return, they explicitly do not subscribe to changes in
+ * the records they don't return.
+ *
+ * Thus, we need to manually patch the request documents stored in the cache
+ * to ensure they update.
+ *
+ * @param store - The store instance
+ * @param todo - The todo that was marked completed
+ */
 export function patchCacheTodoDeleted(store: Store, todo: SavedTodo) {
   const resourceKey = keyForResource(todo);
+  const allRequestKey = keyForRequest(store, getAllTodos());
   const completedRequestKey = keyForRequest(store, getCompletedTodos());
   const activeRequestKey = keyForRequest(store, getActiveTodos());
 
-  // Remove from both active and completed
+  // Remove from "all"
+  store.cache.patch({
+    record: allRequestKey,
+    op: 'remove',
+    value: resourceKey,
+    field: 'data',
+  });
+  // Remove from "active"
   store.cache.patch({
     record: activeRequestKey,
     op: 'remove',
     value: resourceKey,
     field: 'data',
   });
+  // Remove from "completed"
   store.cache.patch({
     record: completedRequestKey,
     op: 'remove',
