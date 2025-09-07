342501d97bc65f754a72c7584826bb908b2379d7
Krystan HuffMenne <kmenne+github@gmail.com>
2025-09-07 15:52:18 -0700

    --wip-- [skip ci]

--- apps/emberjs/app/components/fixme/paginate/-private/types.ts
+++
@@ -1,5 +0,0 @@
-import type { ReactiveDataDocument } from '@warp-drive/core/reactive';
-
-// A type that can infer T from ReactiveDataDocument<T[]>
-export type InferDocumentArrayType<D> =
-  D extends ReactiveDataDocument<(infer T)[]> ? T : never;
